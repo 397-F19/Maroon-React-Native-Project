22167419517b5e2f50775b46c1783cef05f5b613
Owen Zhang <sizhengzhang2021@u.northwestern.edu>
2019-11-18 14:28:50 -0600

    put components into folder

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
-import ScreenOne from './ScreenOne.js'
+import ScreenOne from './src/ScreenOne.js'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
