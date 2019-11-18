46a8f3817264b1498dcccb730f7a8413f8e6a5a0
Jordan Nishina <jordannishina2021@u.northwestern.edu>
2019-11-17 20:22:47 -0600

    successfully fetches data from BetterDoctorAPI

--- App.js
+++ App.js
@@ -11,11 +11,22 @@ const instructions = Platform.select({
 export default class App extends Component {
   constructor(props) {
     super(props);
+    
+    this.state = {doctorJSON: []};
+
   }
+
+  fetchJSON = async (lat,long) => {
+    const url = 'https://api.betterdoctor.com/2016-03-01/doctors?location='+ lat + ',' + long + ',100&skip=2&limit=10&user_key=e98def16c263c71592c3c2f74e24097a'
+    const response = await fetch(url).then((response)=> response.json()).then((response)=> response.data);
+    // console.log(typeof(response))
+    // console.log(response)
+    this.setState({ doctorJSON: response });
+  }
+  
   render() {
-    var doctorJSON = []
     return (
-      <ScreenOne/>
+      <ScreenOne fetchDoctors={this.fetchJSON} doctorData={this.state.doctorJSON}/>
     );
   }
 }
