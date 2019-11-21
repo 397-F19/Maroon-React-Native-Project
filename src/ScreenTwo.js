3bf8b9a06b49c8a47277c8e0e63b7edb7edd97dd
adigreg <ariannadigregorio2020@u.northwestern.edu>
2019-11-20 21:23:01 -0600

    cards don't quite work

--- src/ScreenTwo.js
+++ src/ScreenTwo.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Image, Text, View } from 'react-native';
 import { Card, Button } from 'react-native-material-ui';
 
+const classes = StyleSheet.create(theme => ({
+    grid: {
+      marginLeft: 250,
+      marginTop: 75,
+      paddingLeft: 60,
+    },
+    card:{
+      display:"flex",
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      width: 450,
+      height: 250,
+      paddingTop: 20,
+    },
+    content:{
+      display:"flex",
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+    }
+  }));
+
 const ScreenTwo = ({doctorData}) => {
-    console.log(doctorData);
     return(
-        <View />
+        <View>
+        {doctorData.map(doctor =>
+          (
+             <Card>
+               <Card.Media
+                   image={<Image source={doctor.profile.image_url} />}
+                   overlay
+               />
+                <Card.Body>
+                <Text>{doctor.profile.first_name + " " + doctor.profile.last_name}</Text>
+                </Card.Body>               
+             </Card>
+
+        ))}
+        </View>
     );
 
 }
