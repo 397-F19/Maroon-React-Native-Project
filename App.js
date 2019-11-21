8fcfe5ac4087dd4e445488029ddfb7a234b543ef
Owen Zhang <sizhengzhang2021@u.northwestern.edu>
2019-11-21 01:08:11 -0600

    save progress

--- App.js
+++ App.js
@@ -1,42 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect} from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
-import ScreenOne from './src/ScreenOne.js'
-import ScreenThree from './src/ScreenThree.js'
+import ScreenOne from './src/ScreenOne.js';
+import ScreenThree from './src/ScreenThree.js';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
 
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {doctorJSON: []};
-    this.pageState = 3;
-
+const App = () => {
+  const [json, setjson] = useState([])
+  const [page, setpage] = useState(3)
+  const [review, setreview] = React.useState({});
+  const [doc,setdoc] = React.useState('');
+  if (page == 1)
+  {
+    return(
+      <ScreenOne jsonstate={{json, setjson}} pagestate={{page, setpage}}/>
+    )
   }
-
-  fetchJSON = async (lat,long) => {
-    const url = 'https://api.betterdoctor.com/2016-03-01/doctors?location='+ lat + ',' + long + ',100&skip=2&limit=10&user_key=e98def16c263c71592c3c2f74e24097a'
-    const response = await fetch(url).then((response)=> response.json()).then((response)=> response.data);
-    // console.log(typeof(response))
-    // console.log(response)
-    this.setState({ doctorJSON: response });
-  }
-  
-  render() {
-    if (this.pageState==1){
-    return (
-      <ScreenOne fetchDoctors={this.fetchJSON} doctorData={this.state.doctorJSON}/>
-    );
-  }
-    else if(this.pageState==3){
-      return(
-        <ScreenThree />
-      )
-    }
+  else if (page == 3){
+    return(
+      <ScreenThree pagestate={{page, setpage}} jsonstate={{json, setjson}} settingdoctor = {{doc,setdoc}} reviewstate = {{review, setreview}}/>
+    )
   }
 }
 
+export default App;
--- src/ScreenThree.js
+++ src/ScreenThree.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect} from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Divider, Button, StyleSheet, Text, View ,Image} from 'react-native';
+import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-material-cards';
 
 const styles = StyleSheet.create({
     container: {
@@ -11,18 +12,60 @@ const styles = StyleSheet.create({
       },
 })
 const ScreenThree = ({pagestate,settingdoctor,reviewstate}) => {
-    const [openrating, setOpenrating] = React.useState(false);
-    const [openreview, setOpenreview] = React.useState(false);
+    const [openrating, setOpenrating] = useState(false);
+    const [openreview, setOpenreview] = useState(false);
     const handleClickOpen = () => {
         setOpenrating(true);
       };
-      const handleClose = () => {
-        setOpenrating(false);
-      };
+    const handleClose = () => {
+      setOpenrating(false);
+    };
+  
+    const handleReviewClick= () =>{
+      setOpenreview(!openreview);
+    }
+
+  var practicesSet = new Set();
+  settingdoctor.doc.practices.map(practices=>practicesSet.add(practices.name));
+  var insuranceSet = new Set();
+  settingdoctor.doc.insurances.map(insurance=>insuranceSet.add(insurance.insurance_plan.name));
+  return (
+    <View style={styles.container}>
+    <Card>
+      <CardImage 
+        source={{uri:settingdoctor.doc.profile.image_url}} 
+        title="Above all i am here"
+      />
+      <CardContent text={settingdoctor.doc.profile.first_name + " " + settingdoctor.doc.profile.last_name}/>
+    </Card>
     
-      const handleReviewClick= () =>{
-        setOpenreview(!openreview);
-      }
+    <Text>
+      <Text style={{fontSize: 18, fontStyle: 'italic', marginBottom: 10}}>Biography</Text>
+      <Divider/>
+      {settingdoctor.doc.profile.bio}
+    </Text>
+
+    <Text style={{marginTop:60}}>
+      <Text style={{fontSize: 18, fontStyle: 'italic', marginBottom: 10}}>Practices</Text>
+      <Divider/>
+      {Array.from(practicesSet).map(practices =>
+      <Text>{practices}</Text>
+      )}
+    </Text>
+    
+    <Text style={{marginTop:60}}>
+      <Text style={{fontSize: 18, fontStyle: 'italic', marginBottom: 10}}>Insurance Plans Taken</Text>
+      <Divider/>
+      {Array.from(insuranceSet).map(insurance =>
+      <Text>{insurance}</Text>
+      )}
+    </Text>
+
+    <Button style={{margin: 40, float:'right'}} onPress={function(event){pagestate.setpage(2)}}>go back</Button>
+
+    </View>
+  )
+    
     return(
     <View style={styles.container}>
         <Text>This is ScreenThree</Text>
