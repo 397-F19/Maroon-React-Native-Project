0dca2d287b1d398c46347cc497d996cb8061ad6c
Panyang Qi <qipanyang@126.com>
2019-11-21 17:06:08 -0600

    switch among three pages

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { Component, useState, useEffect} from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import ScreenOne from './src/ScreenOne.js';
 import ScreenTwo from './src/ScreenTwo.js';
+import ScreenThree from './src/ScreenThree.js';
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -11,15 +13,26 @@ const instructions = Platform.select({
 const App = () => {
   const [json, setjson] = useState([])
   const [page, setpage] = useState(1)
+  const [doc, setdoc] = useState('');
+  const [review, setreview] = React.useState({});
+
   if (page == 1)
   {
+    console.log(page)
     return(
       <ScreenOne jsonstate={{json, setjson}} pagestate={{page, setpage}}/>
     )
   }
   else if (page == 2){
+    console.log(page)
     return(
-      <ScreenTwo doctorData={json}/>
+      <ScreenTwo jsonstate={{json, setjson}} pagestate={{page, setpage}} settingdoctor={{doc,setdoc}}/>
+    )
+  }
+  else{
+    console.log(page)
+    return (
+      <ScreenThree pagestate={{page,setpage}} settingdoctor={{doc,setdoc}} reviewstate = {{review, setreview}}/>
     )
   }
 }
--- src/ScreenTwo.js
+++ src/ScreenTwo.js
@@ -1,14 +1,95 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { Card, Button } from 'react-native-material-ui';
+import { Platform, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-material-ui';
+import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-material-cards';
+// import { Dropdown } from 'react-native-material-dropdown';
+// import {Drawer} from 'react-native-drawer';
 
-const ScreenTwo = ({doctorData}) => {
-    console.log(doctorData);
+const useStyles = StyleSheet.create({
+    qdTitle: {
+        fontSize: 40,
+        textAlign: 'center',
+        paddingTop: 40,
+        paddingBottom: 6,
+        color: '#FFFFFF'
+      },
+      qdDesc: {
+        fontSize: 16,
+        textAlign: 'center',
+        fontStyle: 'italic',
+        margin: 5,
+        color: '#FFFFFF',
+        paddingBottom: 6,
+      },
+      head: {
+        backgroundColor: 'rgba(87, 137, 255, 100)',
+      }
+  });
+
+const classes = StyleSheet.create(theme => ({
+    grid: {
+      marginLeft: 250,
+      marginTop: 75,
+      paddingLeft: 60,
+    },
+    card:{
+      display:"flex",
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      width: 450,
+      height: 250,
+      paddingTop: 20,
+    },
+    content:{
+      display:"flex",
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+    }
+  }));
+
+const ScreenTwo = ({jsonstate, pagestate, settingdoctor}) => {
+    const [open, setOpen] = React.useState(false);
+    const handleDrawerOpen = () => {
+      setOpen(true);
+    };
+    doctorData = jsonstate.json
+    // console.log(doctorData)
+    // const switch_page = ({num}) => {
+    //     pagestate.setpage(1)
+    //   }
     return(
-        <View />
+        <ScrollView>
+        <View style={useStyles.head}>
+          <Text style={useStyles.qdTitle}>QuickDoc</Text>
+          <Text style={useStyles.qdDesc}>Information on local doctors at your fingertips.</Text>
+          <Button text="Filter" onPress={handleDrawerOpen}/>
+        </View>
+        {/* <Drawer
+        //    className={useStyles.drawer}
+            variant="persistent"
+            anchor="left"
+            open={open}
+            tapToClose={true}
+            classes={{
+          //  paper: useStyles.drawerPaper,
+            }}
+        /> */}
+        {doctorData.map(doctor =>
+          (
+             <Card key={doctor.profile.first_name + " " + doctor.profile.last_name}>
+              <CardImage 
+                source={{uri: doctor.profile.image_url}} 
+              />
+              <CardContent text={doctor.profile.first_name + " " + doctor.profile.last_name}/>
+              <CardButton title="View Doctor Bio" color="blue" onPress={function(event){pagestate.setpage(3);settingdoctor.setdoc(doctor);}}></CardButton>
+            </Card>
+        ))}
+        <Button text="Go Back" color="blue" onPress={function(event){pagestate.setpage(1)}}></Button>
+        </ScrollView>
     );
 
 }
 
 export default ScreenTwo;
-
